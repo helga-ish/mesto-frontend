f1130e92415e213a3bd45223d363106051f181b7
Helga Ish <belfegor.nomnom@gmail.com>
2023-05-19 17:06:48 +0800

    feat: Login component created

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PageWithForm from "./PageWithForm";
+
+export default function Login() {
+
+    return(
+        <PageWithForm
+        name = 'login'
+        title = 'Вход'
+        buttonTitle= 'Войти'
+        />
+    )
+} 
