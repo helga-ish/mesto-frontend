fe5e0e63fe44871b1f6d081f032127f65135f50a
Helga Ish <belfegor.nomnom@gmail.com>
2023-05-21 00:14:05 +0800

    feat: url changed, authorize added

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,7 @@
-export const BASE_URL = 'https://api.nomoreparties.co';
+export const BASE_URL = 'https://auth.nomoreparties.co';
 
 export const register = (email, password) => {
-  return fetch(`${BASE_URL}/auth/local/register`, {
+  return fetch(`${BASE_URL}/signup`, {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
@@ -17,3 +17,22 @@ export const register = (email, password) => {
   })
   .catch((err) => console.log(err));
 };
+
+export const authorize = (identifier, password) => {
+    return fetch(`${BASE_URL}/signin`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({identifier, password})
+    })
+    .then((response => response.json()))
+    .then((data) => {
+      if (data.user){
+        localStorage.setItem('jwt', data.jwt);
+        return data;
+      }
+    })
+    .catch(err => console.log(err))
+  };
