f08f4c5c85ebb9e747e89f6f0d526dae8a20382e
Helga Ish <belfegor.nomnom@gmail.com>
2023-05-21 00:11:49 +0800

    feat: InfoToolTip modal window created

---
+++ src/components/InfoTooltip.js
@@ -0,0 +1,28 @@
+import React from "react";
+import success from '../images/sign-up-success.svg';
+import fail from '../images/sign-up-fail.svg'
+
+export default function InfoTooltip({isSucceed, isOpen, onClose}) {
+
+    const className = `popup ${isOpen ? 'popup_active' : ''}`;
+
+    return(
+        <section className={className} id="popup-signup">
+            <div className="popup__container">
+            <button className="popup__toggle" type="button" onClick={onClose}></button>
+                {isSucceed ? (
+                    <div>
+                        <img className="popup__signup-sign" src={success} alt="успешная регистрация"/>
+                        <p className="popup__signup-message">Вы успешно зарегистрировались!</p>
+                    </div>
+                ) : (
+                    <div>
+                        <img className="popup__signup-sign" src={fail} alt="неуспешная регистрация"/>
+                        <p className="popup__signup-message">Что-то пошло не так! Попробуйте ещё раз.</p>
+                    </div>
+                )}
+            </div>
+        </section>
+                
+    )
+}
