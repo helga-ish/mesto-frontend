69e680cc4d5252529b657a98b6376691ab5e071d
Helga Ish <belfegor.nomnom@gmail.com>
2023-05-20 21:02:13 +0800

    feat: onSubmit commented

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,12 +2,22 @@ import React from "react";
 import PageWithForm from "./PageWithForm";
 
 export default function Login() {
+    // const [isLoggedIn, setIsLoggedIn] = React.useState(null);
+
+    // function handleSubmit(e) {
+    //     e.preventDefault();
+    //     return onUpdateUser({
+    //         name: nameUser,
+    //         about: description,
+    //       });
+    // }
 
     return(
         <PageWithForm
         name = 'login'
         title = 'Вход'
         buttonTitle= 'Войти'
+        // onSubmit={handleSubmit}
         />
     )
 } 
