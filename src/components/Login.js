9c2282f4a221435946fc2d0d486c52a53c38d97c
Helga Ish <belfegor.nomnom@gmail.com>
2023-05-21 00:12:08 +0800

    feat: Login function created

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,51 @@
 import React from "react";
 import PageWithForm from "./PageWithForm";
+import { useNavigate } from "react-router-dom";
+import * as auth from '../utils/auth.js';
 
-export default function Login() {
-    // const [isLoggedIn, setIsLoggedIn] = React.useState(null);
-
-    // function handleSubmit(e) {
-    //     e.preventDefault();
-    //     return onUpdateUser({
-    //         name: nameUser,
-    //         about: description,
-    //       });
-    // }
+export default function Login({handleLogin}) {
+    const [formValue, setFormValue] = React.useState({
+        email: '',
+        password: ''
+      })
+      
+      const navigate = useNavigate();
+    
+      const handleChange = (e) => {
+        const {name, value} = e.target;
+    
+        setFormValue({
+          ...formValue,
+          [name]: value
+        });
+      }
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!formValue.email || !formValue.password){
+          return;
+        }
+        auth.authorize(formValue.email, formValue.password)
+          .then((data) => {
+          if (data.jwt) {
+                  setFormValue({email: '', password: ''});
+          handleLogin();
+          navigate('/', {replace: true})
+          }
+    
+          })
+          .catch(err => console.log(err));
+      }
+    
 
     return(
         <PageWithForm
         name = 'login'
         title = 'Вход'
         buttonTitle= 'Войти'
-        // onSubmit={handleSubmit}
+        onSubmit={handleSubmit}
+        onChange={handleChange}
+        formValueEmail={formValue.email}
+        formValuePassword={formValue.password}
         />
     )
 } 
