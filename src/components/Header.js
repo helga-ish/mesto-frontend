87749770a4eae45a47ade9f99ee491197e2b4d5d
Helga Ish <belfegor.nomnom@gmail.com>
2023-05-21 22:44:25 +0800

    feat: suggested commented changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import logo from '../images/logo.svg';
+// import { Link } from 'react-router-dom';
 
-function Header() {
+function Header({children}) {
     return(
         <header className="header">
             <img className="logo" src={logo} alt="логотип mesto" />
+            {children}
+            {/* {!onRegister ? (
+                <Link className='header__link' to="/sign-up">Зарегистрироваться</Link>
+            ) : (
+                <Link className='header__link' to="/sign-in">Войти</Link>
+            )} */}
         </header>
     )
 }
