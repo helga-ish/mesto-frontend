60d0f4d6d3cdea79dd5f55e9800eef8130b1c0ea
Helga Ish <belfegor.nomnom@gmail.com>
2023-05-21 19:26:26 +0800

    feat: identifier => email in authorize

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -4,7 +4,7 @@ export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
     method: 'POST',
     headers: {
-      'Accept': 'application/json',
+    //   'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({email, password})
@@ -18,14 +18,14 @@ export const register = (email, password) => {
   .catch((err) => console.log(err));
 };
 
-export const authorize = (identifier, password) => {
+export const authorize = (email, password) => {
     return fetch(`${BASE_URL}/signin`, {
       method: 'POST',
       headers: {
-        'Accept': 'application/json',
+        // 'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({identifier, password})
+      body: JSON.stringify({email, password})
     })
     .then((response => response.json()))
     .then((data) => {
