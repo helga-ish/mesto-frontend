7916de8e51d8159fd2d101072234563ffed110c3
Helga Ish <belfegor.nomnom@gmail.com>
2023-05-22 18:11:20 +0800

    feat: routes removed, outlet added

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,17 +1,9 @@
 
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
-const ProtectedRoute = ({ component: Component, ...props  }) => {
-    return (
-      <Routes>
-      <Route exact>
-        {
-          () => props.loggedIn ? <Component {...props} /> : <Navigate to="/sign-in" replace/>
-        }
-      </Route>
-      </Routes>
-  )}
+const ProtectedRoute = (props) => {
+  return props.loggedIn ? <Outlet /> : <Navigate to="/sign-in" replace/>;
+  };
 
   export default ProtectedRoute;
