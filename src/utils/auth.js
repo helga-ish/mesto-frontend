109f6eb50e5eceff044ebdaa8750aaecd16924e6
Helga Ish <belfegor.nomnom@gmail.com>
2023-05-22 20:14:54 +0800

    feat: getEmail added, authorize .then changed

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -4,7 +4,6 @@ export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
     method: 'POST',
     headers: {
-    //   'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({email, password})
@@ -22,17 +21,28 @@ export const authorize = (email, password) => {
     return fetch(`${BASE_URL}/signin`, {
       method: 'POST',
       headers: {
-        // 'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({email, password})
     })
     .then((response => response.json()))
     .then((data) => {
-      if (data.user){
-        localStorage.setItem('jwt', data.jwt);
+        localStorage.setItem('token', data.token);
         return data;
-      }
     })
     .catch(err => console.log(err))
+  };
+
+  export const getEmail = (token) => {
+    return fetch(`${BASE_URL}/users/me`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        "Authorization" : `Bearer ${token}`
+      }
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
   };
