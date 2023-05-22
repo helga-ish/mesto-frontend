d63dd34b8f82d613af62e79ab2e00083420f3d9f
Helga Ish <belfegor.nomnom@gmail.com>
2023-05-22 20:13:24 +0800

    feat: location dependencies added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,31 @@
 import logo from '../images/logo.svg';
-// import { Link } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import React from 'react';
+// import * as auth from '../utils/auth.js';
 
-function Header({children}) {
+function Header({loggedIn, email}) {
+
+
+const location = useLocation();
+// console.log(location);
+
+
+
     return(
         <header className="header">
             <img className="logo" src={logo} alt="логотип mesto" />
-            {children}
-            {/* {!onRegister ? (
-                <Link className='header__link' to="/sign-up">Зарегистрироваться</Link>
+
+            {location.pathname === '/sign-up' ? (
+            <Link className='header__link' to="/sign-in">Войти</Link>) :
+            loggedIn ? (
+                <div>
+                    <p className='header__email'>{ email }</p>
+                    <Link className='header__link' to="/">Выйти</Link>
+                </div>
             ) : (
-                <Link className='header__link' to="/sign-in">Войти</Link>
-            )} */}
+                    <Link className='header__link' to="/sign-up">Зарегистрироваться</Link>
+            )
+            }
         </header>
     )
 }
